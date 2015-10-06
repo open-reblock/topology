ef7fea5714e4a2b2d608b996f8019cc34ac69eec
Joe Hand <joe@joeahand.com>
2015-10-06 14:58:20 -0600

    remove errant plot function

--- my_graph_helpers.py
+++ my_graph_helpers.py
@@ -1016,8 +1016,6 @@ def import_and_setup(filename, threshold=1, component=None,
     original map.
     """
 
-    plt.close('all')
-
     # check that rezero is an array of len(2)
     # check that threshold is a float
 
