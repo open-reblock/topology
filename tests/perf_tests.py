7a6ca8569673b87f81c70b03a161365072529695
satej soman <satej.soman@gmail.com>
2019-02-28 23:38:07 -0600

    set up perf tests

---
+++ tests/perf_tests.py
@@ -0,0 +1,43 @@
+import logging
+import timeit 
+import subprocess
+from graph import my_graph as mg
+from graph import my_graph_helpers as mgh
+
+DEFAULT_THRESHOLD = 1
+DEFAULT_BLOCKING  = True
+
+def get_git_hash():
+    return subprocess.check_output(["git", "describe", "--always"]).strip()
+
+def test_case(filename, name):
+    logger = logging.getLogger()
+    logger.disabled = True
+    graph = mgh.import_and_setup(filename=filename, 
+        threshold=DEFAULT_THRESHOLD, 
+        byblock=DEFAULT_BLOCKING, 
+        name=name,
+        logger=logger)
+    return len(graph.connected_components())
+
+def main(names): 
+    git_hash = get_git_hash()
+    for name in names:
+        print git_hash, name,
+        blocks = test_case("data/" + name, name)
+        time_taken = timeit.timeit('test_case("data/" + name, name)', 
+            setup='from __main__ import test_case; name = "%s"' % name, 
+            number=3)
+        print blocks, time_taken, blocks/time_taken
+
+if __name__ == '__main__':
+    names = [
+        'Phule_Nagar_v6',
+        'CapeTown',
+        'Epworth_Before',
+        'Epworth_demo',
+        'Las_Vegas',
+        'NYC',
+        'Prague'
+    ]
+    main(names)
